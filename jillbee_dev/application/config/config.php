8d682c4007ac847825707a01da0c773838346861
ryan <ryan@ryankoon.com>
2015-09-15 00:50:22 -0700

    test

--- jillbee_dev/application/config/config.php
+++ jillbee_dev/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/dev/jillbee_ci/main'; // HAI BRADY
+$config['base_url'] = 'http://localhost/dev/jillbee_ci/main'; // HAI BRADY 2
 
 /*
 |--------------------------------------------------------------------------
