2cfcd808df46aefbe964edb3fd00acc4f38037ad
Braden Koon <koon.braden@gmail.com>
2015-09-15 00:36:40 -0700

    test

--- jillbee_ci/application/config/config.php
+++ jillbee_ci/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/dev/jillbee_ci/main'; // abc
+$config['base_url'] = 'http://localhost/dev/jillbee_ci/main'; // abcasdf
 
 /*
 |--------------------------------------------------------------------------
