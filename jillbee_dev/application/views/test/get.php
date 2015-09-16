88b2130c6666eb64d5777aaadcc946f61c21c2a5
ryan <ryan@ryankoon.com>
2015-09-15 20:30:54 -0700

    Added testing controller with post and get views for running unit tests
    Signed-off-by: ryan <ryan@ryankoon.com>

---
+++ jillbee_dev/application/views/test/get.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<div>
+	<h2>Unit Test <?php echo $title; ?></h2>
+	<span><?php echo $description; ?></span>
+	<div>
+		<iframe src="<?php echo $url;?>"></iframe>
+	</div>
+</div>
---
+++ jillbee_dev/application/views/test/require/top.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html>
+	<head>
+
+	</head>
+	<body>
+		<h1><?php echo $title; ?></h1.
