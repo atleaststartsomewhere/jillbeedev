a87146dc2c08a47c6606987e7993501e32d6f3df
ryan <ryan@ryankoon.com>
2015-09-15 18:15:51 -0700

    API REFACTOR: -Deprecated the old api endpoint (rename api.php to api_deprecated) -Added api directory with locations endpoint

---
+++ jillbee_dev/application/controllers/api/locations.php
@@ -0,0 +1,40 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// LOCATIONS API CONTROLLER
+// : index
+class Locations extends REST_Controller {
+
+	function Locations()	{
+		parent::__construct();
+		$this->load->model('location');
+	}
+	//---------------------------------------------------------------------------------------------------------------
+	// index [get]
+	//		No parameters returns all locations in database
+	//		Otherwise supplied parameters narrow results by client or filter the results
+	//---------------------------------------------------------------------------------------------------------------
+	// : client (optional)
+	// : order (optional)
+	// : enabled (optional)
+	//-------------------------
+	public function index_get() {
+		// TO DO: Client ID will be encrypted, so decrypt and send to model
+		if ( $this->get('client') )
+		{
+			$locations = $this->location->get_client_locations($this->get('client'), $this->get('order'), $this->get('enabled'));
+			$this->response(array("result" => "success", "locations" => $locations), 200);
+			return;
+		}
+		else
+		{
+			$locations = $this->location->get_locations($this->get('order'), $this->get('enabled'));
+			$this->response(array("result" => "success", "locations" => $locations), 200);      
+			return;
+		}
+	}
+}
